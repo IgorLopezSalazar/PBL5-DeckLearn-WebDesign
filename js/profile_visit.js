b7cdd2fe8c6de133865104f8fa8398d56b4ee96b
heiko <heikomorales2@gmail.com>
2021-12-29 21:07:24 +0100

    fixed button

--- js/profile_visit.js
+++ js/profile_visit.js
@@ -2,7 +2,6 @@ var button;
 
 window.onload = function(){
     button = document.getElementById("button-profile_visit");
-    console.log(button.textContent);
     button.addEventListener('mouseout', following);
     button.addEventListener('mouseover', unfollow);
     button.addEventListener('click', follow);
@@ -10,10 +9,8 @@ window.onload = function(){
 
 function following() {
     button.innerHTML = "Following";
-    console.log(button.textContent);
 
     button.classList.remove("button-unfollow");
-    button.classList.remove("button-follow");
     button.classList.add("button-following");
 
     button.removeEventListener('click', following);
@@ -26,23 +23,19 @@ function unfollow() {
     button.classList.add("button-unfollow");
 
     button.classList.add("button-unfollow");
-    button.classList.remove("button-follow");
     button.classList.remove("button-following");
 
     button.innerHTML = "Unfollow";
-    console.log(button.textContent);
 }
 
 function follow() {    
     button.innerHTML = "Follow";
     
     button.classList.remove("button-unfollow");
-    button.classList.add("button-follow");
     button.classList.remove("button-following");
 
     button.removeEventListener('mouseout', following);
     button.removeEventListener('mouseover', unfollow);
     button.removeEventListener('click', follow);
     button.addEventListener('click', following);
-    console.log(button.textContent);
 }
